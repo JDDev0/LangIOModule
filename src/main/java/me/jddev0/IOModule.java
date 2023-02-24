7f58c19f85ed52aa50a91546d376dcbd85a42e46
JDDev0 <jddev0@dorneger.at>
2023-02-24 11:24:51 +0100

    Add "fp.existsFile()"

--- src/main/java/me/jddev0/IOModule.java
+++ src/main/java/me/jddev0/IOModule.java
@@ -47,6 +47,17 @@ public class IOModule extends LangNativeModule {
 			return null;
 		})));
 
+		exportFunctionPointerVariableFinal("existsFile", createDataObject(new DataObject.FunctionPointerObject((FileFunctionPointer1Arg)(interpreter, fileID, INNER_SCOPE_ID) -> {
+			LangInterpreterInterface lii = new LangInterpreterInterface(interpreter);
+
+			DataObject errorObject;
+			if((errorObject = checkFileOpened(lii, fileID, INNER_SCOPE_ID)) != null)
+				return errorObject;
+
+			File file = openedFiles.get(fileID);
+			return createDataObject(file.exists());
+		})));
+
 		return null;
 	}
 	
